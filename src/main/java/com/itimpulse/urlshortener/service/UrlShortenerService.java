484f31bcd7edabcdd1ed1aa11e27c8be3ec0e04f
katros1 <katros250@gmail.com>
2025-07-07 10:23:13 +0200

    apply spotless lint format

--- src/main/java/com/itimpulse/urlshortener/service/UrlShortenerService.java
+++ src/main/java/com/itimpulse/urlshortener/service/UrlShortenerService.java
@@ -17,7 +17,6 @@ import java.util.regex.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
